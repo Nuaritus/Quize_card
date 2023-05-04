f3e69251cf19c70330da2c46b5ded44fb324042e
Artem Berezkin <berezkingg@gmail.com>
2023-05-04 17:02:25 +0300

    firstcommit

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require("@babel/register");
 const express = require("express");
-// const indexRouter = require("./routes/index.route");
-// const path = require("path");
+const indexRouter = require("./routes/index.route");
+const path = require("path");
 const app = express();
 
-const PORT = 3001;
+const PORT = 3000;
 // app.use(express.urlencoded({ excendet: true }));
 // app.use(express.json());
 
-// app.use("/", indexRouter);
+app.use("/", indexRouter);
 
 app.listen(PORT, () => {
   console.log(`Ура сервер на ${PORT} работает`);
